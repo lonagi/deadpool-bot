597de5cb3b7a470fe5daddecc341b957be1dd274
Sasha <40319171+lonagi@users.noreply.github.com>
2021-10-23 21:23:35 +0300

    auto mount

--- music.sh
+++ music.sh
@@ -6,3 +6,14 @@ chsh -s /bin/zsh
 apt install davfs2 mpg321 -y
 mkdir /mnt/dav
 mount -t davfs -o noexec url /mnt/dav/
+umount /mnt/dav
+
+cat << EOF | sudo tee -a /etc/fstab
+https://nextcloud.example.com/remote.php/webdav/ /mnt/dav davfs _netdev,noauto,user,uid=timelord,gid=timelord 0 0
+EOF
+
+cat << EOF | sudo tee -a /etc/davfs2/secrets
+/mnt/dav timelord mypassword
+EOF
+
+mount /mnt/dav
