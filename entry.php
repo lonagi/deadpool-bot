aee5457f6f5376068bca895054a412c636c08b27
Sasha <40319171+lonagi@users.noreply.github.com>
2021-10-24 14:41:49 +0300

    Update entry.php

--- entry.php
+++ entry.php
@@ -5,7 +5,7 @@ if($_SERVER['REMOTE_ADDR']=="" and isset($_POST))
   if(isset($_POST['htype']) and isset($_POST['value'])) {
     include "../rb.php";
 
-    R::setup('mysql:host=localhost;dbname=deadpool', "","")
+    R::setup('mysql:host=localhost;dbname=deadpool', "","");
     $stat = R::dispense("history"); 
     $stat->htype = $_POST['htype'];
     $stat->timestamp = time();
