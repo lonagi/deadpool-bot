2377717477507566c9faf758a21773f344af0ab3
Sasha <40319171+lonagi@users.noreply.github.com>
2021-10-24 14:37:44 +0300

    Create entry.php

---
+++ entry.php
@@ -0,0 +1,16 @@
+<?php
+
+if($_SERVER['REMOTE_ADDR']=="" and isset($_POST))
+{
+  if(isset($_POST['htype']) and isset($_POST['value'])) {
+    include "../rb.php";
+
+    R::setup('mysql:host=localhost;dbname=deadpool', "","")
+    $stat = R::dispense("history"); 
+    $stat->htype = $_POST['htype'];
+    $stat->timestamp = time();
+    $stat->value = $_POST['value'];
+    R::store($stat);
+  }
+}
+?>
