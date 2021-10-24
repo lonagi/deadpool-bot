917f0dd943f1806212913119abfa95b10ee3def7
Sasha <40319171+lonagi@users.noreply.github.com>
2021-10-24 12:43:45 +0300

    admin privs

--- bot_tg.py
+++ bot_tg.py
@@ -15,6 +15,7 @@ except:
 bot = telebot.TeleBot(nvgconfgtoken)
 
 MAIN_PATH = "/mnt/dav/Music"
+ADMIN = ""
 
 def get_proc():
     stream_cmd = os.popen("pgrep -f mpg321")
@@ -50,79 +51,87 @@ def navigate(bot, message, path, pref = "i"):
     
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    ccs = ("/music", '/loop', '/volume', '/stop', '/pulse', '/mount')
-    for i in ccs:
-        itembtn = types.KeyboardButton(i)
-        markup.add(itembtn)
-    bot.send_message(message.chat.id, "Выбери команду", reply_markup=markup)
+    if ADMIN == message.chat.id:
+        markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        ccs = ("/music", '/loop', '/volume', '/stop', '/pulse', '/mount')
+        for i in ccs:
+            itembtn = types.KeyboardButton(i)
+            markup.add(itembtn)
+        bot.send_message(message.chat.id, "Выбери команду", reply_markup=markup)
     
 @bot.message_handler(commands=['mount'])
 def mount(message):
-    c = "mount /mnt/dav"
-    os.system(c)
-    bot.send_message(message.chat.id, f"$ {c}")
+    if ADMIN == message.chat.id:
+        c = "mount /mnt/dav"
+        os.system(c)
+        bot.send_message(message.chat.id, f"$ {c}")
     
 @bot.message_handler(commands=['pulse'])
 def pulse(message):
-    c = "pulseaudio -D"
-    os.system(c)
-    bot.send_message(message.chat.id, f"$ {c}")
+    if ADMIN == message.chat.id:
+        c = "pulseaudio -D"
+        os.system(c)
+        bot.send_message(message.chat.id, f"$ {c}")
     
 @bot.message_handler(commands=['kill','stop','стоп'])
 def kill(message):
-    pids = get_proc()
-    if pids:
-        for pid in pids:
-            os.system(f"kill {pid}")
-    bot.send_message(message.chat.id, f"Stop")
+    if ADMIN == message.chat.id:
+        pids = get_proc()
+        if pids:
+            for pid in pids:
+                os.system(f"kill {pid}")
+        bot.send_message(message.chat.id, f"Stop")
     
 @bot.message_handler(commands=['volume',"громкость"])
 def volume(message):
-    m = message.text.split(" ")[-1]
-    with open("./volume.txt","w") as f:
-        f.write(m)
-    bot.send_message(message.chat.id, f"Ok")
+    if ADMIN == message.chat.id:
+        m = message.text.split(" ")[-1]
+        with open("./volume.txt","w") as f:
+            f.write(m)
+        bot.send_message(message.chat.id, f"Ok")
 
 @bot.message_handler(commands=['loop',"повтор"])
 def loops(message):
-    m = message.text.split(" ")[-1]
-    with open("./loops.txt","w") as f:
-        f.write(m)
-    bot.send_message(message.chat.id, f"Ok")
+    if ADMIN == message.chat.id:
+        m = message.text.split(" ")[-1]
+        with open("./loops.txt","w") as f:
+            f.write(m)
+        bot.send_message(message.chat.id, f"Ok")
 
 @bot.message_handler(commands=['music','музыка','m',"м"])
-def music(message):    
-    navigate(bot, message, MAIN_PATH)
+def music(message):
+    if ADMIN == message.chat.id:
+        navigate(bot, message, MAIN_PATH)
     
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    if call.message:
-        if call.data[0] == "i":
-            f = call.data[1:]
-            navigate(bot, call.message, f"{MAIN_PATH}/{f}", "j")
-        elif call.data[0] == "j":
-            j = json.loads(call.data[1:])
-            f,d,s = [],[],[]
-            for (dirpath, dirnames, filenames) in os.walk(j[1]):
-                d.extend(dirpath)
-                f.extend(dirnames)
-                s.extend(filenames)
-                break
-            file = s[j[0]].replace(" ","\ ")
-            with open("./volume.txt","r") as f:
-                getvolume=int(f.read())
-            with open("./loops.txt","r") as f:
-                loops=int(f.read())
-            c = f"pulseaudio -D & mpg321 {j[1]}/{file} -K -v -l {loops} -g {getvolume} --stereo"
-            os.system(c)
-            bot.send_message(call.message.chat.id, c)
-            
-            markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-            ccs = ('/stop',)
-            for i in ccs:
-                itembtn = types.KeyboardButton(i)
-                markup.add(itembtn)
-            bot.send_message(message.chat.id, "Управляй", reply_markup=markup)
+    if ADMIN == message.chat.id:
+        if call.message:
+            if call.data[0] == "i":
+                f = call.data[1:]
+                navigate(bot, call.message, f"{MAIN_PATH}/{f}", "j")
+            elif call.data[0] == "j":
+                j = json.loads(call.data[1:])
+                f,d,s = [],[],[]
+                for (dirpath, dirnames, filenames) in os.walk(j[1]):
+                    d.extend(dirpath)
+                    f.extend(dirnames)
+                    s.extend(filenames)
+                    break
+                file = s[j[0]].replace(" ","\ ")
+                with open("./volume.txt","r") as f:
+                    getvolume=int(f.read())
+                with open("./loops.txt","r") as f:
+                    loops=int(f.read())
+                c = f"pulseaudio -D & mpg321 {j[1]}/{file} -K -v -l {loops} -g {getvolume} --stereo"
+                os.system(c)
+                bot.send_message(call.message.chat.id, c)
+
+                markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+                ccs = ('/stop',)
+                for i in ccs:
+                    itembtn = types.KeyboardButton(i)
+                    markup.add(itembtn)
+                bot.send_message(message.chat.id, "Управляй", reply_markup=markup)
             
 bot.polling(none_stop=True)
