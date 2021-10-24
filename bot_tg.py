c6d24d8e2864943ff932b5b3caa97ede91811ef3
Sasha <40319171+lonagi@users.noreply.github.com>
2021-10-24 17:16:12 +0300

    Update bot_tg.py

--- bot_tg.py
+++ bot_tg.py
@@ -11,15 +11,16 @@ import datetime, re, json, os, json, time
 try:
     import nvgconfg
     nvgconfgtoken = nvgconfg.token
+    ADMIN = nvgconfg.admin
+    API_URL = nvgconfg.api
 except:
     pass
 
 MAIN_PATH = "/mnt/dav/Music"
-ADMIN = ""
 bot = telebot.TeleBot(nvgconfgtoken)
 
 def add_stat(htype, value):
-    req.post("/deadpool/entry.php", data={"htype":htype, "value":value})
+    req.post(f"{API_URL}/deadpool/entry.php", data={"htype":htype, "value":value})
 
 def get_proc():
     stream_cmd = os.popen("pgrep -f mpg321")
@@ -49,16 +50,17 @@ def runfile(folder, file):
 def playfile(message, folder, file):
     markup = get_stop_markup()
     c = runfile(folder, file)
-    os.system(c)
     
     if folder:
         fpath = f"{folder}/{file}"
     else:
         fpath = f"{file}"
-    fpath = fpath.replace("\ "," ")
+    fpath = fpath.replace("\ "," ").replace("\&","&")
     
     #bot.send_message(message.chat.id, c, reply_markup=markup)
     bot.send_message(message.chat.id, f"Включаю {fpath}", reply_markup=markup)
+    __kill()
+    os.system(c)
 
 def get_all_music():
     s = []
@@ -86,16 +88,18 @@ def navigate(bot, message, path, pref = "i"):
         m = "Выбери файл"
         mm = s
         
-    keyboard = types.InlineKeyboardMarkup()
-    p = 0
-    for i in mm:
-        if pref == "j":
-            callback_button = types.InlineKeyboardButton(text=str(i), callback_data=f"{pref}{json.dumps([p,path])}")
-        elif pref == "i":
-            callback_button = types.InlineKeyboardButton(text=str(i), callback_data=f"{pref}{i}")
-        keyboard.add(callback_button)
-        p += 1
-    bot.send_message(message.chat.id, m, reply_markup=keyboard)
+    for page in range(1,len(mm)//50+2):
+        keyboard = types.InlineKeyboardMarkup()
+        p = 0
+        for i in mm[(page-1)*50:page*50+1]:
+            if pref == "j":
+                callback_button = types.InlineKeyboardButton(text=str(i), callback_data=f"{pref}{json.dumps([p,page,path])}")
+            elif pref == "i":
+                callback_button = types.InlineKeyboardButton(text=str(i), callback_data=f"{pref}{i}")
+            keyboard.add(callback_button)
+            p += 1
+        bot.send_message(message.chat.id, m, reply_markup=keyboard)
+        time.sleep(1)
     
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -112,73 +116,78 @@ def mount(message):
     if ADMIN == str(message.chat.id):
         c = "mount /mnt/dav"
         os.system(c)
-        bot.send_message(message.chat.id, f"$ {c}")
         add_stat("mount","dav")
+        bot.send_message(message.chat.id, f"$ {c}")
     
 @bot.message_handler(commands=['pulse'])
 def pulse(message):
     if ADMIN == str(message.chat.id):
         c = "pulseaudio -D"
         os.system(c)
-        bot.send_message(message.chat.id, f"$ {c}")
         add_stat("pulse","-D")
+        bot.send_message(message.chat.id, f"$ {c}")
         
 @bot.message_handler(commands=['play'])
 def play(message):
     if ADMIN == str(message.chat.id):
         m = message.text.split(" ")
         if len(m) > 1:
-            m = m[-1].replace(" ","\ ")
-            playfile(message, None, m)
+            m = " ".join(m[1:]).replace(" ","\ ").replace("&","\&")
             add_stat("music_play_play",m)
+            playfile(message, None, m)
         else:
             add_stat("music_play_error","play")
             bot.send_message(message.chat.id, f"Нет такого файла")
 
+def __find(message):
+    m = message.text.split(" ")
+    if len(m) > 1:
+        m = " ".join(m[1:]).replace(" ","\ ").replace("&","\&")
+        markup = types.ReplyKeyboardRemove(selective=False)
+        bot.send_message(message.chat.id, "Ищу музыку...", reply_markup=markup)
+
+        s = get_all_music()
+        song = process.extractOne(m,s)
+        add_stat("music_play_find",m)
+        playfile(message, None, song[0].replace(" ","\ ").replace("&","\&"))
+    else:
+        add_stat("music_play_error","find")
+        bot.send_message(message.chat.id, f"Нет такого пути")
+            
 @bot.message_handler(commands=['find'])
 def find(message):
     if ADMIN == str(message.chat.id):
-        m = message.text.split(" ")
-        if len(m) > 1:
-            m = m[-1]
-            s = get_all_music()
-            print(s)
-            song = process.extractOne(m,s)
-            playfile(message, None, song)
-            add_stat("music_play_find",m)
-        else:
-            add_stat("music_play_error","find")
-            bot.send_message(message.chat.id, f"Нет такого пути")
+        __find(message)
         
 @bot.message_handler(commands=['shutdown'])
 def shutdown(message):
     if ADMIN == str(message.chat.id):
         c = "shutdown"
         os.system(c)
-        bot.send_message(message.chat.id, f"$ {c}")
         add_stat("shutdown","berry")
+        bot.send_message(message.chat.id, f"$ {c}")
     
 @bot.message_handler(commands=['reboot'])
 def reboot(message):
     if ADMIN == str(message.chat.id):
         c = "reboot"
         os.system(c)
-        bot.send_message(message.chat.id, f"$ {c}")
         add_stat("reboot","berry")
+        bot.send_message(message.chat.id, f"$ {c}")
         
 def __kill():
     pids = get_proc()
     if pids:
         for pid in pids:
-            os.system(f"убиваю процесс {pid}")
+            os.system(f"kill {pid}")
     
 @bot.message_handler(commands=['kill','stop','стоп'])
 def kill(message):
     if ADMIN == str(message.chat.id):
+        add_stat("music_stop","stop")
         __kill()
         markup = types.ReplyKeyboardRemove(selective=False)
         bot.send_message(message.chat.id, "Стоп", reply_markup=markup)
-        add_stat("music_stop","stop")
     
 @bot.message_handler(commands=['volume',"громкость"])
 def volume(message):
@@ -217,23 +226,26 @@ def callback_inline(call):
         if call.message:
             if call.data[0] == "i":
                 f = call.data[1:]
-                chat_id=call.message.chat.id, message_id=call.message.message_id
+                bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id, timeout=None)
                 navigate(bot, call.message, f"{MAIN_PATH}/{f}", "j")
             elif call.data[0] == "j":
-                __kill()
-                markup = get_stop_markup()
-                bot.send_message(call.message.chat.id, "Включаю...", reply_markup=markup)
                 bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id, timeout=None)
                 
                 j = json.loads(call.data[1:])
                 f,d,s = [],[],[]
-                for (dirpath, dirnames, filenames) in os.walk(j[1]):
+                for (dirpath, dirnames, filenames) in os.walk(j[2]):
                     d.extend(dirpath)
                     f.extend(dirnames)
                     s.extend(filenames)
                     break
-                file = s[j[0]].replace(" ","\ ")
-                playfile(call.message, j[1], file)
-                add_stat("music_play_nav", f"{MAIN_PATH}/{j[1]}/{file}")
+                file = s[50*(j[1]-1)+j[0]].replace(" ","\ ").replace("&","\&")
+                add_stat("music_play_nav", f"{j[2]}/{file}")
+                playfile(call.message, j[2], file)
+
+@bot.message_handler(content_types=['text'])
+def siri_find(message):
+    if ADMIN == str(message.chat.id):
+        if message.text.split(" ")[0] in ("Найди", "Включи", "Включаю", "Вруби"):
+            __find(message)
             
 bot.polling(none_stop=True)
